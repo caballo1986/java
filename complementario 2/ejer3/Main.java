79d1531d35db261afcbf31e68a967a95cabf3bcf
caballo1986 <cesaro.aquino@gmail.com>
2021-06-14 16:55:01 -0300

    entrega
    entrega de complemetarios

---
+++ complementario 2/ejer3/Main.java
@@ -0,0 +1,31 @@
+package ejer3;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+/**
+ *
+ * @author Aquino Cesar
+ */
+public class Main {
+
+    public static void main(String[] args) {
+      List <Integer> cartas = new ArrayList<>();
+      
+      cartas.add(2);
+      cartas.add(3);
+      cartas.add(4);
+      cartas.add(5);
+      cartas.add(6);
+      cartas.add(7);
+      cartas.add(8);
+      cartas.add(9);
+      cartas.add(10);
+
+      System.out.println(cartas);
+      Collections.reverse(cartas);
+      System.out.println(cartas); 
+      Collections.shuffle(cartas);
+      System.out.println(cartas); 
+
+    }
+}
---
+++ complementario 2/ejer4/Main.java
@@ -0,0 +1,13 @@
+package ejer4;
+/**
+ *
+ * @author Aquino Cesar
+ */
+public class Main {
+    public static void main(String[] args) {
+        Alumno a = new Alumno();
+        a.crearAuxiliar();
+        a.dividirEnGrupos();
+        a.mostrarGrupos();
+    }
+}
